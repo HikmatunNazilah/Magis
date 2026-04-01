81e8037b48cfc361760d6f205787b45d2fbdfeac
denis <denissetiaji.dst@gmail.com>
2026-04-01 16:03:18 +0700

    Configure database and Vercel deployment

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
 // Configuration for Database Connection
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASSWORD');
-$db   = getenv('DB_NAME');
-$port = getenv('DB_PORT');
+$host = getenv('DB_HOST') ?: '103.30.147.68';
+$user = getenv('DB_USER') ?: 'sekelikn_magis_usr';
+$pass = getenv('DB_PASSWORD') ?: '[]pl--Xt3)0-!WP[';
+$db   = getenv('DB_NAME') ?: 'sekelikn_magis_db';
+$port = getenv('DB_PORT') ?: '3306';
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
